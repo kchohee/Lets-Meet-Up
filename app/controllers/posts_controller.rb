fa4ca5d67c020fa6084a5ca493380692a5e70acf
kchohee <56492609+kchohee@users.noreply.github.com>
2020-03-08 14:55:00 -0500

    posts route to edit and show
    fixed issue of route for edit and the show view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,12 +20,11 @@ end
 post "/posts/new" do
     if logged_in?
         @user = User.find_by(:id=>session[:user_id])
-         @user.id
-        # @post = Post.create(params)
-        # @post.published = Time.new
-        # @post.user = @user
-        # @post.save
-        # redirect "/posts/#{@post.id}"
+        @post = Post.create(params)
+        @post.published = Time.new
+        @post.user = @user
+        @post.save
+        redirect "/posts/#{@post.id}"
     else
         erb :"posts/error"
      end
@@ -34,21 +33,33 @@ end
 get "/posts/:id" do
     if logged_in?
         @user = User.find_by(:id=>session[:user_id])
-        @post = Post.find_by(params)
-        erb :"posts/show"
+        @post = Post.find_by(:id=>params[:id])
+        erb :"/posts/show"
+     else
+       redirect "/login"
+     end
+end
+
+get "/posts/:id/edit" do
+    if logged_in?
+        @post = Post.find_by(:id=>params[:id])
+        @user = User.find_by(:id=>session[:user_id])
+        erb :"/posts/edit"
     else
         redirect "/login"
     end
 end
 
-# get "/posts/:id/edit" do
-#     if logged_in?
-#         @post = Post.find_by(params)
-    
-# end
-
 patch "/posts/:id/edit" do
-    
+    @user = User.find_by(:id=>session[:user_id])
+    @post = Post.find_by(:id=>params[:id])
+    if @post.user != @user
+        erb :'/posts/error'
+    else
+        @post.content = params[:content]
+        @post.save
+        redirect "/posts/#{@post.id}"
+    end
 end
 
 get "/posts/:id/delete" do
